fe84b4565d8a2174526fa345240ff871deed5906
Dan Pasanen <dan.pasanen@gmail.com>
2013-02-28 09:14:49 -0600

    Switch to symlinked partition names and change the worker script name
    The symlinked partition names are less likely to brick a device
    if we use the actual partition numbers, someone could inadvertently
    flash something to their bootloader or somewhere they don't intend.
    
    Change the worker script's name since it does more than mkbootimg

--- kernel/kernel_flash.sh
+++ kernel/kernel_flash.sh
@@ -1,7 +1,7 @@
 #!/sbin/sh
 cd /tmp/
-/tmp/dd if=/dev/block/mmcblk0p7 of=/tmp/boot.img
+/tmp/dd if=/dev/block/platform/msm_sdcc.1/by-name/boot of=/tmp/boot.img
 /tmp/unpackbootimg /tmp/boot.img
 /tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline 'androidboot.hardware=qcom user_debug=31' --base 0x80200000  --ramdiskaddr 0x81500000 -o /tmp/newboot.img
-/tmp/dd if=/tmp/newboot.img of=/dev/block/mmcblk0p7
+/tmp/dd if=/tmp/newboot.img of=/dev/block/platform/msm_sdcc.1/by-name/boot
 busybox chmod 644 /system/lib/modules/*
