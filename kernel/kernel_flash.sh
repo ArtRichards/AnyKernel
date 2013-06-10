641fde070f741317484e7bfa5e77d34bf8b0aafa
Dan Pasanen <dan.pasanen@gmail.com>
2013-06-10 18:54:51 -0500

    make it ouya!

--- kernel/kernel_flash.sh
+++ kernel/kernel_flash.sh
@@ -1,7 +1,7 @@
 #!/sbin/sh
 cd /tmp/
-/tmp/dd if=/dev/block/platform/msm_sdcc.1/by-name/boot of=/tmp/boot.img
+/tmp/dd if=/dev/block/platform/sdhci-tegra.3/by-name/LNX of=/tmp/boot.img
 /tmp/unpackbootimg /tmp/boot.img
-/tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline 'androidboot.hardware=qcom user_debug=31' --base 0x80200000  --ramdiskaddr 0x81500000 -o /tmp/newboot.img
-/tmp/dd if=/tmp/newboot.img of=/dev/block/platform/msm_sdcc.1/by-name/boot
+/tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --base 0x10000000 -o /tmp/newboot.img
+/tmp/dd if=/tmp/newboot.img of=/dev/block/platform/sdhci-tegra.3/by-name/LNX
 busybox chmod 644 /system/lib/modules/*
